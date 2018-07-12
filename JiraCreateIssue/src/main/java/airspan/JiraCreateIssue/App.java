35a7639c2148941a5e3969a88080f7d537aff3ec
oingber <oingber@airspan.com>
2018-07-12 13:18:56 +0300

    a

--- JiraCreateIssue/src/main/java/airspan/JiraCreateIssue/App.java
+++ JiraCreateIssue/src/main/java/airspan/JiraCreateIssue/App.java
@@ -1,7 +1,6 @@
 package airspan.JiraCreateIssue;
 
 import java.io.IOException;
-
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -14,33 +13,31 @@ import okhttp3.Response;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
     	jiraRestCreateIssue();
     }
     
-    private static void jiraRestCreateIssue() {//10.3.10.49
+    private static void jiraRestCreateIssue() {
 		String jiraIP = "10.3.10.49";
 		String jiraPort = "8080";
-    	String setupName = "SWIT-8";
-		String epicKey = "SW-348";
-		String projectId = "10603";//SW2
-		String[] issues = { "SWIT-1 Regression Analysis",
-							"SWIT-6 Regression Analysis",
-							"SWIT-32 Sanity Analysis",
-							"SWIT-25 Sanity Analysis",
-							"SWIT-26 Sanity Analysis",
-							"SWIT-34 Sanity Analysis",
-							"SWIT-35 Sanity Analysis",
-							"SWIT-37 Sanity Analysis",
-							"SWIT-31 Sanity Analysis",
-							"SWIT-8 Sanity Analysis" };
+		//String projectId = "10603";//SW2
+		String projectId = "10604";//SW1
+		String[] issues = { "SWIT-5 Sanity Analysis",
+							"SWIT-10 Sanity Analysis",
+							"SWIT-33 Sanity Analysis",
+							"SWIT-36 Sanity Analysis",
+							"SWIT-30 Regression Analysis",
+							"SWIT-29 Regression Analysis",
+							"SWIT-2 Regression Analysis",
+							};
 		
-		int[] epics = new int[]{370,371,372,373,374,375,376,377,378,348};
+		int[] epics = new int[]{421,372,379,351,442,443,444};
+		int[] time = new int[]{2,2,2,2,4,4,4};
 		
 		for (int i = 0; i < issues.length; i++) {
 
-			for (int j = 0; j < 20; j++) {
+			for (int j = 0; j < 22; j++) {
 
 				try {
 					OkHttpClient client = new OkHttpClient();
@@ -49,11 +46,12 @@ public class App
 					RequestBody body = RequestBody.create(mediaType,
 							"{\n    \"fields\": {\n       \"project\":\n       {\n          \"id\": \"" + projectId
 									+ "\"\n       },\n       \"summary\": \""+issues[i]+"\",\n       \"components\": [{ \"name\" : \"Analysis\"}],\n       \"customfield_10000\" : "
-									+ "\"SW2-"+(epics[i])+"\",\n       \"timetracking\": {\"originalEstimate\": \"2h\"},\n       \"issuetype\": {\n          \"name\": \"Story\"\n       }\n   }\n}");
+									+ "\"SW2-"+(epics[i])+"\",\n       \"timetracking\": {\"originalEstimate\": \""+time[i]+"h\"},\n       \"issuetype\": {\n          \"name\": \"Story\"\n       }\n   }\n}");
 					
+					// .addHeader("Authorization", "Basic aGdvbGRidXJkOnN1YmFydTk3")  //user hen
 					Request request = new Request.Builder().url("http://"+jiraIP+":"+jiraPort+"/rest/api/2/issue/").post(body)
 							.addHeader("Content-Type", "application/json")
-							.addHeader("Authorization", "Basic aGdvbGRidXJkOnN1YmFydTk3")//user hen
+							.addHeader("Authorization", "Basic b2luZ2JlcjpvcmVuMTIzNA==")//user yuri
 							.addHeader("Cache-Control", "no-cache")
 							.addHeader("Postman-Token", "f6c60a69-91a8-4fe8-a9a8-e1fa637d9d14").build();
 
